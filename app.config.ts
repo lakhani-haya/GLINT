ee5616876e420bb37a799d214e3132a5a2d0f65f
Haya Lakhani <157073204+lakhani-haya@users.noreply.github.com>
2025-11-23 21:05:55 -0600

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default ({ config }: { config: any }) => ({
   ...config,
   extra: {
-    // Expose environment variables to 
+    // Expose environment variables
     apiBase: process.env.EXPO_PUBLIC_API_BASE || 'http://localhost:3000',
   },
 });
